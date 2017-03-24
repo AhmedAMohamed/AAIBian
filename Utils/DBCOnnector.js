53ee776bb4165fe7d0c073dbd40fb55f9cc42afb
Ahmed Alaa <ahmedalaa3307023@gmail.com>
2017-03-24 09:48:26 +0200

    initial commit
    Signed-off-by: Ahmed Alaa <ahmedalaa3307023@gmail.com>

---
+++ Utils/DBCOnnector.js
@@ -0,0 +1,35 @@
+
+var connector = function(url, callback) {
+    console.log(url);
+
+    var mysql = require('mysql');
+    //var client = require('../models/client');
+    //var driver = require('../models/driver');
+    //var user = require('../models/user');
+    //var ride = require('../models/ride');
+
+    /*
+
+    mysql.connect(url, function(error) {
+        if(error){
+            console.log("Error");
+            callback(false);
+        }
+        else {
+            callback(true);
+            console.log("DB connected");
+            console.log(mongoose.modelNames());
+        }
+
+        var db = mongoose.connection;
+        db.on('error', console.error.bind(console, 'connection error:'));
+        db.once('open', function() {
+            // we're connected!
+            console.log("HAHAAHAHAHHAHAHAH  AA");
+        });
+
+    });
+    */
+
+}
+module.exports = connector;
