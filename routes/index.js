fbdb81efd9f078d906fc0f1b88194b352e691097
Ahmed Alaa <ahmedalaa3307023@gmail.com>
2017-04-07 11:45:58 +0200

    trial
    Signed-off-by: Ahmed Alaa <ahmedalaa3307023@gmail.com>

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,22 @@
 var express = require('express');
 var router = express.Router();
-
+var mongoose = require('mongoose');
 /* GET home page. */
 router.get('/', function(req, res, next) {
     console.log("here");
-    res.json({"Ahmed":"Hello"});
+    var options = {
+        user: 'alaa',
+        pass: 'ahmed'
+    };
+    var uri = "mongodb://alaa:ahmed@ds153710.mlab.com:53710/aaibian";
+    mongoose.connect(uri, options, function(error) {
+        if(error) {
+            res.json({"Ahmed":"error"});
+        }
+        else {
+            res.json({"Ahmed":"passed"});
+        }
+    });
 });
 
 module.exports = router;
