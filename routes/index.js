4035c906aba36cac20444036dbde6e517a9edec7
Ahmed Alaa <ahmedalaa3307023@gmail.com>
2017-04-11 19:40:53 +0200

    test csv read
    Signed-off-by: Ahmed Alaa <ahmedalaa3307023@gmail.com>

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,14 @@ var router = express.Router();
 var mongoose = require('mongoose');
 var user_model = require('../models/user_model');
 /* GET home page. */
-
+var csv = require('fast-csv');
+var fs  = require('fs');
 
 router.get('/', function(req, res, next) {
     console.log("here");
     var options = {
-        user: 'AhmedAMohamed',
-        pass: 'gehad1234'
+        user: 'alaa',
+        pass: 'ahmed'
     };
     var uri = "mongodb://alaa:ahmed@ds153710.mlab.com:53710/aaibian";
     mongoose.connect(uri, options, function(error) {
@@ -25,7 +26,7 @@ router.get('/', function(req, res, next) {
                 approved: false,
             });
             a.save(function (err,op) {
-                if (error) {
+                if (err) {
                     res.json({"Ahmed":err});
                 }
                 else {
@@ -39,4 +40,12 @@ router.get('/', function(req, res, next) {
     });
 });
 
+
+router.get('/trial', function (req, res, next) {
+    fs.createReadStream(process.env.OPENSHIFT_DATA_DIR + 'atm_locations.csv').pipe(csv()).on('data', function (data) {
+        console.log(data);
+    }).on('end', function (data) {
+        console.log("read finished");
+    });
+});
 module.exports = router;
