daa9766ff1874d0865edbec1a5c41b5c2fe1882f
Ahmed Alaa <ahmedalaa3307023@gmail.com>
2017-06-07 14:32:23 +0200

    Upload
    Signed-off-by: Ahmed Alaa <ahmedalaa3307023@gmail.com>

--- routes/creator.js
+++ routes/creator.js
@@ -351,7 +351,13 @@ router.get('/test_notification', function(req, res, next) {
             if (err) {
                 console.log("Something has gone wrong!");
             } else {
+
                 console.log("Successfully sent with response: ", response);
+                res.json({
+                    valid: true,
+                    msg: "Done",
+
+                });
             }
         });
 });
