5cb290e621446d1c2b25421d7b5e71bd76be742e
Ahmed Alaa <ahmedalaa3307023@gmail.com>
2017-06-11 12:16:16 +0200

    Upload tested
    Signed-off-by: Ahmed Alaa <ahmedalaa3307023@gmail.com>

--- routes/admin.js
+++ routes/admin.js
@@ -20,7 +20,7 @@ var News = require('../models/news_model');
 var Privilege = require('../models/privileges_model');
 var helpers = require('../Utils/helpers');
 
-//var sha1 = require('sha1');
+var sha1 = require('sha1');
 var randomstring = require("randomstring");
 var FCM = require('fcm-node');
 
