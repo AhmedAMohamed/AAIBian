e2191bcb676617a82dc5c63a0b25b4cee6aa8e4c
Ahmed Alaa <ahmedalaa3307023@gmail.com>
2017-06-11 17:26:03 +0200

    Trial
    Signed-off-by: Ahmed Alaa <ahmedalaa3307023@gmail.com>

--- app.js
+++ app.js
@@ -5,14 +5,14 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var cors = require('cors');
-
+/*
 var creator = require('./routes/creator');
 var users = require('./routes/users');
 var admins = require('./routes/admin');
 var db_connector = require('./Utils/DBCOnnector');
 var helpers = require('./Utils/helpers');
 var employees = require('./routes/employee');
-
+*/
 var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -32,9 +32,9 @@ db_connector("mongodb://alaa:ahmed@ds161400.mlab.com:61400/aaibian", function (v
        //helpers['starters'].benefit_delete_schedule();
        //helpers['starters'].news_delete_schdule();
        app.use('/', creator, cors());
-       app.use('/aaibian/user', users, cors());
-       app.use('/aaibian/admin', admins, cors());
-       app.use('/aaibian/emp', employees, cors());
+       //app.use('/aaibian/user', users, cors());
+       //app.use('/aaibian/admin', admins, cors());
+       //app.use('/aaibian/emp', employees, cors());
    }
 });
 
