1fc4194b107f59c4bec84115c11ace5bbf079d6b
Ahmed Alaa <ahmedalaa3307023@gmail.com>
2017-07-09 13:47:16 +0300

    Delete Date
    Signed-off-by: Ahmed Alaa <ahmedalaa3307023@gmail.com>

--- Utils/helpers.js
+++ Utils/helpers.js
@@ -77,6 +77,7 @@ var users_helpers = {
                 callback(null);
             }
             else {
+
                 if (us.login_status != reserved_tokens.logout) {
                     callback(us);
                 }
